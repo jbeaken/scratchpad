357fb6b6312eb28dd1805bba9d3152030d8c6b80
jbeaken <j.beaken@elsevier.com>
2022-04-02 20:36:52 +0100

    Getting there

--- src/test/java/org/mzuri/scratchpad/leetcode/KthPalindrome.java
+++ src/test/java/org/mzuri/scratchpad/leetcode/KthPalindrome.java
@@ -4,12 +4,23 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class KthPalindrome {
 
+    public long[] kthPalindrome(int[] queries, int intLength) {
 
-    public int[] initializePalindrome(int palindromeLength) {
+        long[] answers = new long[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            final int[] initialPalindrome = initializePalindrome(intLength);
+            long answer = getAnswer(queries[i], initialPalindrome, intLength);
+            answers[i] = answer;
+        }
+
+        return answers;
+    }
+
+    private int[] initializePalindrome(int palindromeLength) {
 
         //the tail/head with ones and filled with zeros
         int[] palindrome = new int[palindromeLength];
@@ -20,42 +31,36 @@ public class KthPalindrome {
         return palindrome;
     }
 
-    public int[] findPalindromes(int[] queries, int palindromeLength) {
-        final int[] palindrome = initializePalindrome(palindromeLength);
+    private long getAnswer(long sequence, int[] palindrome, int intLength) {
 
-        final int[] answers = generatePalindrome(queries, palindrome);
-
-        return answers;
-    }
-
-    public int[] generatePalindrome(int[] queries, int[] palindrome) {
-        int position = 0;
-        int[] answers = new int[queries.length];
-
-        int nextSequence = queries[position];
+        //quick escape
+//        System.out.println(intLength / 2);
+//        System.out.println(  Math.pow(10, intLength / 2 + 1) );
+        if(sequence > Math.pow(10, intLength / 2 + 1)) return -1;
         int sequenceNumber = 1;
 
         while(true) {
+
+            if(palindrome[0] == 0) {
+                return -1;
+            }
+
             int pos = palindrome.length / 2;
 
-            if(sequenceNumber == nextSequence) {
+            if(sequenceNumber == sequence) {
                 if(palindrome[0] == 0) {
-                    answers[position++] = -1;
+                    return -1;
                 } else {
-                    answers[position++] = getPalindromeAsInteger(palindrome);
+                    return getPalindromeAsInteger(palindrome);
                 }
-                if(position == queries.length) break;
-                nextSequence = queries[position];
             }
 
             incrementPalindrome(palindrome, pos);
             sequenceNumber++;
         }
-
-        return answers;
     }
 
-    private int getPalindromeAsInteger(int[] palindrome) {
+    private long getPalindromeAsInteger(int[] palindrome) {
 
         final StringBuffer stringBuffer = new StringBuffer();
 
@@ -63,11 +68,13 @@ public class KthPalindrome {
             stringBuffer.append( palindrome[i] );
         }
 
-        return Integer.valueOf(stringBuffer.toString());
+        return Long.valueOf(stringBuffer.toString());
     }
 
     private void incrementPalindrome(int[] palindrome, int pos) {
         int length = palindrome.length;
+
+        if(palindrome[0] == 0) return;
 
         //is pos in the middle?
         if(length % 2 != 0 && pos == length / 2) {
@@ -85,7 +92,7 @@ public class KthPalindrome {
                     //end of line
                     return;
                 }
-                incrementPalindrome(palindrome, pos -1);
+                incrementPalindrome(palindrome, pos -2);
             }  else {
                 palindrome[pos] = palindrome[pos] + 1;
                 palindrome[length - 1 - pos] = palindrome[length - 1 - pos] + 1;
@@ -101,14 +108,30 @@ class KthPalindromeTest {
     void test() {
         final KthPalindrome kthPalindrome = new KthPalindrome();
 
-        final List<Integer> queries = List.of(1, 2, 3, 4, 5, 90);
-        int palindromeLength = 3;
+//        final List<Integer> queriesAsList = List.of(1, 2, 3, 4, 5, 90);
+//        int palindromeLength = 3;
 
-        final int[] palindrome = kthPalindrome.findPalindromes(queries.stream().mapToInt(Integer::intValue).toArray(), palindromeLength);
+//        final List<Integer> queriesAsList = List.of(2,201429812,8,520498110,492711727,339882032,462074369,9,7,6);
+//        int palindromeLength = 3;
 
-        System.out.println(Arrays.toString( palindrome ));
+//        final List<Integer> queriesAsList = List.of(696771750,62,47,14,17,192356691,209793716,23,220935614,447911113,5,4,72);
+//        int palindromeLength = 4;
 
-        //assert [101,111,121,131,141,999]
+        final List<Integer> queriesAsList = List.of(31, 119983340, 494857782, 48, 904409366, 65626832, 610586467);
+        int palindromeLength = 15;
+        final List<Long> expectedAsList = List.of(100000303000001l,-1l,-1l,100000474000001l,-1l,756268313862657l,-1l);
 
+
+        final int[] queries = queriesAsList.stream().mapToInt(Integer::intValue).toArray();
+
+        //execute
+        final long[] answers = kthPalindrome.kthPalindrome(queries, palindromeLength);
+
+        System.out.println(Arrays.toString( answers ));
+
+        //assert
+        final long[] expected = expectedAsList.stream().mapToLong(Long::longValue).toArray();
+
+        assert Arrays.compare(answers, expected) == 0;
     }
 }
