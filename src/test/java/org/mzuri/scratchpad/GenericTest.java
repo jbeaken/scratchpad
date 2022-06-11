a0ca35c674dba1cda7959ef020795089680143cb
jbeaken <j.beaken@elsevier.com>
2022-06-11 11:20:42 +0000

    Fixing

--- src/test/java/org/mzuri/scratchpad/GenericTest.java
+++ src/test/java/org/mzuri/scratchpad/GenericTest.java
@@ -1,6 +1,7 @@
 package org.mzuri.scratchpad;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -14,35 +15,35 @@ import org.mzuri.scratchpad.generics.Generics;
 @Slf4j
 public class GenericTest {
 
-	List<Animal> animals = List.of(new Dog("woof"));
-	List<Dog> dogs = List.of(new Dog("woof"));
+    List<Animal> animals = List.of(new Dog("woof"));
+    List<Dog> dogs = List.of(new Dog("woof"));
 
-	List<? extends Animal> animalDogs = dogs;
+    List<? extends Animal> animalDogs = dogs;
 
-	Generics generics = new Generics();
+    Generics generics = new Generics();
 
-	@Test
-	public void test() throws IOException {
-		generics.coAnimals(animalDogs);
-		generics.coAnimals(animals);
-		generics.coAnimals(dogs);
+    @Test
+    public void test() throws IOException {
+        generics.coAnimals(animalDogs);
+        generics.coAnimals(animals);
+        generics.coAnimals(dogs);
 
-		generics.contraAnimals(animals);
-		generics.contraAnimals(dogs);
-	}
+        generics.contraAnimals(animals);
+        generics.contraAnimals(dogs);
+    }
 
-	@Test
-	public void testAppend()  {
-		List<Integer> integers = List.of(2, 3, 4);
+    @Test
+    public void testAppend() {
+        List<Integer> integers = new ArrayList<>(List.of(2, 3, 4));
 
-		append(integers, 3);
+        append(integers, 3);
 
-		 log.info( Arrays.toString(integers.toArray()) );
-	}
+        log.info(Arrays.toString(integers.toArray()));
+    }
 
-	public static void append(Collection<Integer> integers, int n) {
-		for (int i = 1; i <= n; i++) {
-			integers.add(i);
-		}
-	}
+    public static void append(Collection<Integer> integers, int n) {
+        for (int i = 1; i <= n; i++) {
+            integers.add(i);
+        }
+    }
 }
