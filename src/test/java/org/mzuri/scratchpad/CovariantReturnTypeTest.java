addd9a85c99bfb3d9fe798f7b99907bc18c4d1c0
jbeaken <j.beaken@elsevier.com>
2022-06-22 22:03:19 +0100

    Adding cats!

--- src/test/java/org/mzuri/scratchpad/CovariantReturnTypeTest.java
+++ src/test/java/org/mzuri/scratchpad/CovariantReturnTypeTest.java
@@ -1,9 +1,24 @@
 package org.mzuri.scratchpad;
 
 import lombok.extern.slf4j.Slf4j;
+import org.mzuri.scratchpad.domain.Animal;
+import org.mzuri.scratchpad.domain.Cat;
+import org.mzuri.scratchpad.domain.Dog;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 public class CovariantReturnTypeTest {
+
+    public static void addDogs(List<? super Animal> list) {
+        list.add(new Dog("tom"));
+    }
+
+    public static void test() {
+        ArrayList<Cat> objects = new ArrayList<>();
+        addDogs(objects);
+    }
 
 }
 
