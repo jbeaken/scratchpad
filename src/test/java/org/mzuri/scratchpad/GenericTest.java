e02c8a1d16b09fc91d5da38055bc681207561417
jbeaken <j.beaken@elsevier.com>
2022-06-23 18:27:51 +0100

    Adding countif

--- src/test/java/org/mzuri/scratchpad/GenericTest.java
+++ src/test/java/org/mzuri/scratchpad/GenericTest.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.function.Predicate;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
@@ -85,12 +86,25 @@ public class GenericTest {
     }
 
     @Test
+    void testAlgorithCountIf() {
+        Collection<Integer> ci = Arrays.asList(1, 2, 3, 4, 5, 7, 9);
+        long count = Algorithm.countIf(ci, i -> i % 2 != 0);
+
+        log.info("count: {}", count);
+    }
+
+    @Test
     void testNode() {
         Node<Circle> nc = new Node<>();
         Node<? extends Shape>  ns = nc;
+    }
 
-        Node2<? extends Compar> node = new Node2<>();
-        Node2<Integer> comp = node;
+    @Test
+    void testInvariance() {
+        List<Animal> animals = new ArrayList<>();
+        animals.add(new Dog());
+        animals.add(new Pet());
+        animals.add(new Cat());
     }
 }
 
@@ -108,6 +122,7 @@ interface ArgumentsProcessor2<X extends Number> {
     X process(X arg1, X arg2);
 }
 
+//Write a generic method to countIf the number of elements in a collection that have a specific property (for example, odd integers, prime numbers, palindromes).
 final class Algorithm {
 
     public static <T> void swap(T[] array, int first, int second) {
@@ -118,16 +133,23 @@ final class Algorithm {
         array[second] = elementFirst;
     }
 
-    public static <T extends Object & Comparable<? super T>> T max(List<T> list, int begin, int end) {
+    public static <T> long countIf(Collection<T> collection, Predicate<T> predicate) {
+        return collection.stream().filter(predicate::test).count();
+    }
 
-        T maxElem = list.get(begin);
+    public static <T extends Comparable<T>> T max(List<T> list, int begin, int end) {
+        T maxElem = list.get(0);
 
-        for (++begin; begin < end; ++begin)
-            if (maxElem.compareTo(list.get(begin)) < 0)
+        for(++begin; begin < end; ++begin) {
+            if (maxElem.compareTo(list.get(begin)) < 0) {
                 maxElem = list.get(begin);
+            }
+        }
 
         return maxElem;
     }
+
+
 }
 class Shape { /* ... */ }
 class Circle extends Shape { /* ... */ }
--- src/test/java/org/mzuri/scratchpad/RunLengthEncoding.java
+++ src/test/java/org/mzuri/scratchpad/RunLengthEncoding.java
@@ -2,7 +2,7 @@ package org.mzuri.scratchpad;
 
 /*
 Run-length encoding is a fast and simple method of encoding strings.
-The basic idea is to represent repeated successive characters as a single count and character.
+The basic idea is to represent repeated successive characters as a single countIf and character.
 For example, the string "AAAABBBCCDAA" would be encoded as "4A3B2C1D2A".
 
 Implement run-length encoding and decoding. You can assume the string to be encoded have no digits and consists solely of alphabetic characters. You can assume the string to be decoded is valid.
