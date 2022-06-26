4d39f6124f074b7180ff0378b18d8007f421a42e
jbeaken <j.beaken@elsevier.com>
2022-06-26 20:41:30 +0100

    Formatting

--- src/main/java/org/mzuri/scratchpad/binarytree/BinaryTree.java
+++ src/main/java/org/mzuri/scratchpad/binarytree/BinaryTree.java
@@ -2,38 +2,29 @@ package org.mzuri.scratchpad.binarytree;
 
 
 public class BinaryTree {
-    // Root of Binary Tree
     Node root;
 
     BinaryTree() { root = null; }
 
-    /* Given a binary tree, print its nodes according to the
-    "bottom-up" postorder traversal. */
+    /* Given a binary tree, print its nodes according to the "bottom-up" postorder traversal. */
     void printPostorder(Node node)	{
         if (node == null) return;
 
-        // first recur on left subtree
         printPostorder(node.left);
 
-        // then recur on right subtree
         printPostorder(node.right);
 
-        // now deal with the node
         print(node);
     }
 
-    // at 1, at 2, at 4, at 5, at null, at null
     /* Given a binary tree, print its nodes in inorder*/
     void printInorder(Node node) {
         if (node == null) return;
 
-        /* first recur on left child */
         printInorder(node.left);
 
-        /* then print the data of node */
         print(node);
 
-        /* now recur on right child */
         printInorder(node.right);
     }
 
@@ -43,13 +34,10 @@ public class BinaryTree {
     void printPreorder(Node node) {
         if (node == null) return;
 
-        /* first print data of node */
         print(node);
 
-        /* then recur on left subtree */
         printPreorder(node.left);
 
-        /* now recur on right subtree */
         printPreorder(node.right);
     }
 
