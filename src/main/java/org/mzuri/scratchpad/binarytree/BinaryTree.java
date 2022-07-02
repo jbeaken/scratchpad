04f0d22e9885f8c4f4af3c97d5ba9ddb2c50c449
jbeaken <j.beaken@elsevier.com>
2022-07-02 16:17:48 +0100

    Adding binary tree tests

--- src/main/java/org/mzuri/scratchpad/binarytree/BinaryTree.java
+++ src/main/java/org/mzuri/scratchpad/binarytree/BinaryTree.java
@@ -1,47 +1,110 @@
 package org.mzuri.scratchpad.binarytree;
 
 
+import java.util.Stack;
+
 public class BinaryTree {
     Node root;
+    StringBuilder result = new StringBuilder();
 
     BinaryTree() { root = null; }
 
-    /* Given a binary tree, print its nodes according to the "bottom-up" postorder traversal. */
+    /* Given a binary tree, record its nodes according to the "bottom-up" postorder traversal. */
     //4,5,2,3,1
-    void printPostorder(Node node)	{
+    void postOrder(Node node)	{
         if (node == null) return;
 
-        printPostorder(node.left);
-        printPostorder(node.right);
-        print(node);
+        postOrder(node.left);
+        postOrder(node.right);
+        record(node);
     }
 
-    /* Given a binary tree, print its nodes in inorder*/
+    /* Given a binary tree, record its nodes in inorder*/
     //4,2,5,1,3
-    void printInorder(Node node) {
+    void inOrder(Node node) {
         if (node == null) return;
 
-        printInorder(node.left);
-        print(node);
-        printInorder(node.right);
+        inOrder(node.left);
+        record(node);
+        inOrder(node.right);
     }
 
-    /* Given a binary tree, print its nodes in preorder*/
+    /* Given a binary tree, record its nodes in preorder*/
     //1,2,4,5,3
-    void printPreorder(Node node) {
+    void preOrder(Node node) {
         if (node == null) return;
 
-        print(node);
-        printPreorder(node.left);
-        printPreorder(node.right);
+        record(node);
+        preOrder(node.left);
+        preOrder(node.right);
     }
 
     // Wrappers over above recursive functions
-    void printPostorder() { printPostorder(root); }
-    void printInorder() { printInorder(root); }
-    void printPreorder() { printPreorder(root); }
+    void postOrder() { postOrder(root); }
+    void inOrder() { inOrder(root); }
+    void preOrder() { preOrder(root); }
 
-    private void print(Node node) {
-        System.out.print(node.key + " ");
+    private void record(Node node) {
+        result.append(node.key + " ");
     }
+
+    public void traverseInOrderWithoutRecursion() {
+        Stack<Node> stack = new Stack<>();
+        Node current = root;
+
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            Node top = stack.pop();
+            record(top);
+            current = top.right;
+        }
+    }
+
+    public void traversePreOrderWithoutRecursion() {
+        Stack<Node> stack = new Stack<>();
+        Node current;
+        stack.push(root);
+        while(! stack.isEmpty()) {
+            current = stack.pop();
+            record(current);
+
+            if(current.right != null)
+                stack.push(current.right);
+
+            if(current.left != null)
+                stack.push(current.left);
+        }
+    }
+
+    public void traversePostOrderWithoutRecursion() {
+        Stack<Node> stack = new Stack<>();
+        Node prev = root;
+        Node current;
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            current = stack.peek();
+            boolean hasChild = (current.left != null || current.right != null);
+            boolean isPrevLastChild = (prev == current.right || (prev == current.left && current.right == null));
+
+            if (!hasChild || isPrevLastChild) {
+                current = stack.pop();
+//                visit(current.value);
+                record(current);
+                prev = current;
+            } else {
+                if (current.right != null) {
+                    stack.push(current.right);
+                }
+                if (current.left != null) {
+                    stack.push(current.left);
+                }
+            }
+        }
+    }
+
 }
--- src/test/java/org/mzuri/scratchpad/binarytree/BinaryTreeTest.java
+++ src/test/java/org/mzuri/scratchpad/binarytree/BinaryTreeTest.java
@@ -1,27 +1,46 @@
 package org.mzuri.scratchpad.binarytree;
 
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Slf4j
 public class BinaryTreeTest {
 
     BinaryTree tree = new BinaryTree();
 
-    @Test
-    void testDepthFirst() {
+    @BeforeEach
+    void beforeAll() {
         tree.root = new Node(1);
         tree.root.left = new Node(2);
         tree.root.right = new Node(3);
         tree.root.left.left = new Node(4);
         tree.root.left.right = new Node(5);
+    }
 
-        System.out.println("Preorder traversal of binary tree is ");
-        tree.printPreorder();
+    @Test
+    void testDepthFirstRecursion() {
+        tree.preOrder();
+        assertEquals(tree.result.toString(),"5 4 3 3 2");
 
-        System.out.println("\nInorder traversal of binary tree is ");
-        tree.printInorder();
+        tree.inOrder();
 
-        System.out.println("\nPostorder traversal of binary tree is ");
-        tree.printPostorder();
+        tree.postOrder();
+    }
+
+    @Test
+    void testDepthFirstStack() {
+        log.info("Preorder traversal of binary tree is ");
+        tree.traversePreOrderWithoutRecursion();
+
+        log.info("\nInorder traversal of binary tree is ");
+        tree.traverseInOrderWithoutRecursion();
+
+        log.info("\nPostorder traversal of binary tree is ");
+        tree.traversePostOrderWithoutRecursion();
     }
 }
