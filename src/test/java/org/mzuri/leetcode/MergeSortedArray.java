17a17aedc076d44463cd7fa80db285646fd1b612
jbeaken <jbeaken@elsevier.com>
2023-01-22 12:19:04 +0000

    Final version

--- src/test/java/org/mzuri/leetcode/MergeSortedArray.java
+++ src/test/java/org/mzuri/leetcode/MergeSortedArray.java
@@ -50,7 +50,7 @@ public class MergeSortedArray {
         int j = n - 1;
         int k = m + n - 1;
 
-        while ( j >= 0) {
+        while (j >= 0) {
             if (i >=0 && nums1[i] > nums2[j]) {
                 nums1[k--] = nums1[i--];
             } else {
