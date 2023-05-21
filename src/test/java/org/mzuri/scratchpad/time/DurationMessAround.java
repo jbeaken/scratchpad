02969a6866d97e4dd1ed36304f5eb6788590f5f9
jbeaken <jbeaken@elsevier.com>
2023-05-21 19:57:12 +0100

    Trying out Duration

--- src/test/java/org/mzuri/scratchpad/time/DurationMessAround.java
+++ src/test/java/org/mzuri/scratchpad/time/DurationMessAround.java
@@ -1,7 +1,11 @@
 package org.mzuri.scratchpad.time;
 
+import java.time.Duration;
+
 public class DurationMessAround {
 
     public static void main(String[] args) {
+        Duration duration = Duration.ofDays(3);
+        System.out.println(duration.toHours());
     }
 }
